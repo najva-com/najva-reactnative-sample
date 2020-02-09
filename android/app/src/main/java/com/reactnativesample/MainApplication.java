4795fbb41fc99e357cff59d8f6198830a73b3e3f
MahdiHT <mht0440232@gamil.com>
2020-02-09 12:22:30 +0330

    Change import of NajvaPackage

--- android/app/src/main/java/com/reactnativesample/MainApplication.java
+++ android/app/src/main/java/com/reactnativesample/MainApplication.java
@@ -12,7 +12,7 @@ import com.facebook.soloader.SoLoader;
 import java.util.Arrays;
 import java.util.List;
 
-import ir.najva.najvasdk.reactnativeplugin.CustomPackage;
+import ir.najva.najvasdk.reactnativeplugin.NajvaPackage;
 
 
 public class MainApplication extends Application implements ReactApplication {
